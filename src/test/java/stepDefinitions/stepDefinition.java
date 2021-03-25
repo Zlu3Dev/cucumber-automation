9dd528f98826d871f2207872127d1d9a807f8a01
Zhiyong <zhiyong.lu@paradyme.us>
2021-03-25 10:47:59 -0400

    update webDriver

--- src/test/java/stepDefinitions/stepDefinition.java
+++ src/test/java/stepDefinitions/stepDefinition.java
@@ -20,9 +20,11 @@ public class stepDefinition {
         /*
         using HeadLess options
         * */
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--headless", "--window-size=1920,1200");
         System.setProperty("webdriver.chrome.driver", "./src/main/resources/chromedriver/chromedriver.exe");
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless", "--disable-gpu", "--window-size=1920,1200",
+                "--ignore-certificate-errors","--disable-extensions",
+                "--no-sandbox","--disable-dev-shm-usage");
         driver = new ChromeDriver(options);
         driver.navigate().to("http://www.paradymemanagement.com/");
     }
